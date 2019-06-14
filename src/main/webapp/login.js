dee2710345c9a8eab477d91ee9262c8ca0ea0e35
Hugh <hughfharding@gmail.com>
2019-06-14 14:26:51 +0100

    One to Many

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -2,8 +2,8 @@ function login(){
 
     let uName = document.getElementById(uName);
     let pass = document.getElementById(pass);
-    fetchData()
-
-
-
-}
+    fetchData("GET","user/",null).then((req)=> {
+        t = req .responseText;
+        sessionStorage.clear();
+        sessionStorage.setItem("userLogin",t);
+    })};
