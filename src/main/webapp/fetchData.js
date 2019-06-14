bdd2c3dda4a5f02a6afd6f9950f7a72bc5ba17a3
Hugh <hughfharding@gmail.com>
2019-06-14 09:35:30 +0100

    IP Address changed

--- src/main/webapp/fetchData.js
+++ src/main/webapp/fetchData.js
@@ -24,7 +24,7 @@ function fetchData(type,ext,sen) {
 
         };
             
-    req.open(type, "http://localhost:8080/holPlanner-1.0/api" + ext);
+    req.open(type, "35.246.36.67/holPlanner-1.0/api" + ext);
     req.setRequestHeader("Content-Type","application/json");
     if(type == "GET" || type == "DELETE"){
             req.send();
