bc87493787af460f5c9b8eacf168059214e18ffc
Hugh <hughfharding@gmail.com>
2019-06-14 11:45:24 +0100

    User db added

--- src/main/java/com/qa/repository/UserDB.java
+++ src/main/java/com/qa/repository/UserDB.java
@@ -5,10 +5,19 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
+import com.qa.model.Destination;
+import com.qa.model.User;
+
 @Transactional(value = TxType.SUPPORTS)
 public class UserDB {
 	
 	@PersistenceContext(unitName = "q")
 	private EntityManager em;
+	
+	@Transactional(value = TxType.REQUIRED)
+	public User create(User user) {
+		em.persist(user);
+		return user;
+	}
 
 }
